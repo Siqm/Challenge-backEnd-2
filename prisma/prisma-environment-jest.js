586665e16ff7f0c9c087a3d7cbccf916704c986d
SiqMello <91890617+Siqm@users.noreply.github.com>
2022-11-01 11:32:12 -0300

    weird import error

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -1,16 +1,31 @@
-const NodeEnvironment = require('jest-environment-node')
+const NodeEnvironment = require('jest-environment-node').TestEnvironment
 const { v4: uuid } = require('uuid')
+const { execSync } = require('child_process')
+const { resolve } = require('path')
+
+const binPath = "./node_modules/.bin/"
+
+require('dotenv').config({
+    path: resolve(__dirname, "..", ".env.test")
+})
 
 class CustomEnvironment extends NodeEnvironment {
 
     constructor(config) {
-        super(config)
-        this.schema = `code_schema_${uuid()}`
-        this.connectionString = `${process.env.DATABASE_URL}${this.schema}`
+        super(config);
+        console.log("database url ", process.env.DATABASE_URL)
+        this.schema = `code_schema_${uuid()}`;
+        console.log("schemas", this.schema);
+        this.connectionString = `${process.env.DATABASE_URL}${this.schema}`;
     }
 
     setup() {
+        process.env.DATABASE_URL = this.connectionString
+        this.global.process.env.DATABASE_URL = this.connectionString
 
+        // Migrations
+        execSync(`cd ${binPath}`)
+        execSync(`prisma migrate dev`)
     }
 }
 
