46e33a3d7f8b7931f4b08cdbc234a83f7b8e0fd5
SiqMello <91890617+Siqm@users.noreply.github.com>
2022-11-11 10:28:25 -0300

    Changes on server

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen(3000, () => console.log("Server Running on port 3000"));
+const port = process.env.PORT;
+app_1.app.listen(port, () => console.log(`Server Running on port ${port}`));
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import { app } from './app';
 
-app.listen(3000, () => console.log("Server Running on port 3000"));
+const port = process.env.PORT
+
+app.listen(port, () => console.log(`Server Running on port ${port}`));
